58aa5b5f85532f9686b7314f22efb0714072b52c
wtl1992 <1050100468@qq.com>
2022-03-04 17:12:16 +0800

    新增httpRequest Cmds

--- ljxwtl/cmd/BaseCmd.go
+++ ljxwtl/cmd/BaseCmd.go
@@ -104,10 +104,142 @@ var (
 			CmdHttpRequestMethod: http.MethodGet,
 		},
 	}
-	RefreshHttpRequestCmd = HttpRequestCmd{
+	CloseWindowHttpRequestCmd = HttpRequestCmd{
 		BaseCmd{
-			CmdName:              "Refresh",
-			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/refresh",
+			CmdName:              "Close Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window",
+			CmdHttpRequestMethod: http.MethodDelete,
+			CmdHttpRequestBody:   "{\n   \n    \"window\" : \"WINDOW_HANDLE\"\n    \n}",
+		},
+	}
+	SwitchToWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Switch To Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n   \n    \"window\" : \"WINDOW_HANDLE\"\n    \n}",
+		},
+	}
+	GetWindowHandleswHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Get Window Handles",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/handles",
+			CmdHttpRequestMethod: http.MethodGet,
+		},
+	}
+	NewWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "New Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/new",
+			CmdHttpRequestMethod: http.MethodPost,
+			/**
+			type: tab、window
+			*/
+			CmdHttpRequestBody: "{\n   \n    \"type\":\"NEW_WINDOW_TYPE\"\n}",
+		},
+	}
+	SwitchToFrameHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Switch To Frame",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/frame",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n   \n    \"id\" : \"FRAME_ID\"\n}",
+		},
+	}
+	SwitchToParentFrameHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Switch To Parent Frame",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/frame/parent",
+			CmdHttpRequestMethod: http.MethodPost,
+		},
+	}
+	GetWindowRectHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Get Window Rect",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/rect",
+			CmdHttpRequestMethod: http.MethodGet,
+		},
+	}
+	SetWindowRectHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Set Window Rect",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/rect",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n    \"width\": WINDOW_RECT_WIDTH,\n    \"height\": WINDOW_RECT_HEIGHT,\n    \"x\": WINDOW_RECT_X\n    \"y\": WINDOW_RECT_Y\n}",
+		},
+	}
+	MaximizeWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Maximize Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/maximize",
+			CmdHttpRequestMethod: http.MethodPost,
+		},
+	}
+	MinimizeWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Minimize Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/minimize",
+			CmdHttpRequestMethod: http.MethodPost,
+		},
+	}
+	FullscreenWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Fullscreen Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/fullscreen",
+			CmdHttpRequestMethod: http.MethodPost,
+		},
+	}
+	FindElementHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Find Element",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/element",
+			CmdHttpRequestMethod: http.MethodPost,
+			/**
+			using:
+			CSS selector	"css selector"
+			Link text selector	"link text"
+			Partial link text selector	"partial link text"
+			Tag name	"tag name"
+			XPath selector	"xpath"
+			*/
+			CmdHttpRequestBody: "{\n    \"using\" :\"LOCATION_STRATEGY\",\n    \"value\":\"SELECT_VALUE\"\n}",
+		},
+	}
+	FindElementsHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Find Elements",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/elements",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n    \"using\" :\"LOCATION_STRATEGY\",\n    \"value\":\"SELECT_VALUE\"\n}",
+		},
+	}
+	FindElementFromElementHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Find Element From Element",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/element/ELEMENT_ID/element",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n    \"using\" :\"LOCATION_STRATEGY\",\n    \"value\":\"SELECT_VALUE\"\n}",
+		},
+	}
+	FindElementsFromElementHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Find Elements From Element",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/element/ELEMENT_ID/elements",
+			CmdHttpRequestMethod: http.MethodPost,
+			CmdHttpRequestBody:   "{\n    \"using\" :\"LOCATION_STRATEGY\",\n    \"value\":\"SELECT_VALUE\"\n}",
+		},
+	}
+	FullscreenWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Fullscreen Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/fullscreen",
+			CmdHttpRequestMethod: http.MethodPost,
+		},
+	}
+	FullscreenWindowHttpRequestCmd = HttpRequestCmd{
+		BaseCmd{
+			CmdName:              "Fullscreen Window",
+			CmdHttpAddress:       "http://localhost:SELENIUM_LISTEN_PORT/session/SESSION_ID/window/fullscreen",
 			CmdHttpRequestMethod: http.MethodPost,
 		},
 	}
